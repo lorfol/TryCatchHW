39430075b83acc8ba6f1f488df295e2ad60d9eab
Владимир Сидоров <lorfol2802@outlook.com>
2018-01-10 16:44:47 +0200

    1

--- TryCatchHW/Program.cs
+++ TryCatchHW/Program.cs
@@ -10,6 +10,46 @@ namespace TryCatchHW
 	{
 		static void Main(string[] args)
 		{
+			byte?[] mass = new byte?[20];
+			FillMass(mass);
+			ShowMass(mass);
+			Console.ReadKey();
+		}
+
+		private static void ShowMass(byte?[] mass)
+		{
+			foreach (var item in mass)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		public static void FillMass(byte?[] mass)
+		{
+			Random rand = new Random();
+
+			for (int i = 0; i < mass.Length; i++)
+			{
+				while (!mass[i].HasValue)
+				{
+					try
+					{
+						mass[rand.Next(-5, 30)] = Convert.ToByte(rand.Next(-50, 300));
+					}
+					catch (IndexOutOfRangeException ex)
+					{
+						Console.WriteLine(ex.Message);
+						continue;
+					}
+					catch (OverflowException ex)
+					{
+						Console.WriteLine(ex.Message);
+						continue;
+					}
+				}
+			}
 		}
 	}
 }
+
+
